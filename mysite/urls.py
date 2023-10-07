a55635d12a76171d103871a117984e8a2605a142
wwszola <wwszola0@gmail.com>
2023-10-07 11:01:58 +0200

    empty url path redirects to home view now

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('', RedirectView.as_view(pattern_name = 'home', permanent = True)),
     path('home/', TemplateView.as_view(template_name = 'home.html'), name = 'home')
 ]
