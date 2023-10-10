b484bf4ee288ecb2a9b642443dbab28dba8b370b
wwszola <wwszola0@gmail.com>
2023-10-10 18:22:17 +0200

    typo fix and better test message content

--- chat/test_views.py
+++ chat/test_views.py
@@ -5,9 +5,9 @@ from .models import Message
 
 
 class PostMessageViewTests(TestCase):
-    def test_anonymous_user_posts_quest_message(self):
+    def test_anonymous_user_posts_guest_message(self):
         post_message_url = reverse("chat:post")
-        message_content = "Test message content."
+        message_content = "test anonymous user posts guest message"
         self.client.post(post_message_url, {"content": message_content})
         message = Message.objects.first()
         self.assertEquals(message.content, message_content)
